30ab9e37cc8daf6a06b42f0d5e3f06606bb343c9
James Mernin <jamespmernin@gmail.com>
2020-12-02 13:57:00 -0500

    Added App code

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,71 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
+
 import './App.css';
+import MainContainer from './containers/MainContainer';
+import Layout from './layouts/Layout';
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+import { loginUser, registerUser, removeToken, verifyUser } from './services/auth';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory()
+
+  useEffect(() => {
+    const handleVerify = async () => {
+      const userData = await verifyUser();
+      setCurrentUser(userData);
+      if (!userData) {
+        history.push('/')
+      }
+    }
+    handleVerify();
+  }, [])
+
+  const handleLogin = async (loginData) => {
+    const userData = await loginUser(loginData);
+    setCurrentUser(userData);
+    history.push('/');
+  }
+
+  const handleRegister = async (registerData) => {
+    const userData = await registerUser(registerData);
+    setCurrentUser(userData);
+    history.push('/');
+  }
+
+  const handleLogout = () => {
+    setCurrentUser(null);
+    localStorage.removeItem('authToken');
+    removeToken();
+    history.push('/');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Layout
+      currentUser={currentUser}
+      handleLogout={handleLogout}
+    >
+      <Switch>
+
+        <Route path='/login'>
+          {/* login */}
+          <Login handleLogin={handleLogin} />
+        </Route>
+
+        <Route path='/signup'>
+          {/* register */}
+          <Register handleRegister={handleRegister} />
+        </Route>
+
+        <Route path='/'>
+          {/* container */}
+          <MainContainer currentUser={currentUser} />
+        </Route>
+
+      </Switch>
+    </Layout>
   );
 }
 
