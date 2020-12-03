4c7e1b8e71dddbe13671ba812b79fc0735756a16
James Mernin <jamespmernin@gmail.com>
2020-12-03 10:26:56 -0500

    Starting Rails app over

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,2 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  before_action :authenticate_user!
-  skip_before_action :authenticate_user!, :only => [:index]
-
-  def after_sign_in_path_for(resource)
-    app_dashboard_index_path
-  end
+class ApplicationController < ActionController::API
 end
