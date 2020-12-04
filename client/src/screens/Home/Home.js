0bb87a5c3b61d2085ac0350b32a61e0d32c75b2a
James Mernin <jamespmernin@gmail.com>
2020-12-04 15:39:35 -0500

    Added to home page

--- client/src/screens/Home/Home.js
+++ client/src/screens/Home/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     <div>
       <h1>Home</h1>
       {/* Need a section for top five on leaderboard, a link to the full leaderboard, a link to make a quiz, a link to take a quiz, a link to edit account, and a link to log out */}
-      {/* Top five scores go here */}
+      {/* Top five scores go here post-MVP */}
       <Link to="/leaderboard">Leaderboard</Link>
       <Link to="/makequiz">Make a Quiz</Link>
       <Link to="/takequiz">Take a Quiz</Link>
