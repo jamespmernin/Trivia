74c41c3c8a643f52c9432e27e4799343ddb3a633
James Mernin <jamespmernin@gmail.com>
2020-12-04 13:55:35 -0500

    Header for home page

--- client/src/screens/Home/Home.js
+++ client/src/screens/Home/Home.js
@@ -5,6 +5,7 @@ import './Home.css';
 const Home = () => {
   return (
     <div>
+      <h1>Home</h1>
       {/* Need a section for top five on leaderboard, a link to the full leaderboard, a link to make a quiz, a link to take a quiz, a link to edit account, and a link to log out */}
       {/* Top five scores go here */}
       <Link to="/leaderboard">Leaderboard</Link>
