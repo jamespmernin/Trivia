d4c2487745e5aaf19876bf4dce40ce055a320107
James Mernin <jamespmernin@gmail.com>
2020-12-06 10:51:29 -0500

    Added quizzes.js

---
+++ client/src/services/quizzes.js
@@ -0,0 +1,46 @@
+import api from './apiConfig'
+
+export const getQuizzes = async () => {
+  try {
+    const response = await api.get('/quizzes')
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const getQuiz = async id => {
+  try {
+    const response = await api.get(`/quizzes/${id}`)
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const createQuiz = async quiz => {
+  try {
+    const response = await api.post('/quizzes', quiz)
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const updateQuiz = async (id, product) => {
+  try {
+    const response = await api.put(`/quizzes/${id}`, quiz)
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const deleteQuiz = async id => {
+  try {
+    const response = await api.delete(`/quizzes/${id}`)
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
